758ec851f9081c19c930bdb031532efba406b4c7
Karan Sharma <83858496+imksh@users.noreply.github.com>
2024-08-19 11:23:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function home(){
+            document.getElementById("homename").style.color ="rgb(245, 74, 74)";
+            document.getElementById("aboutname").style.color = "white";
+            document.getElementById("portfolioname").style.color = "white";
+            document.getElementById("homename").style.textDecoration = "underline";
+            document.getElementById("aboutname").style.textDecoration = "none";
+            document.getElementById("portfolioname").style.textDecoration = "none";
+}
+
+function about(){
+    document.getElementById("aboutname").style.color ="rgb(245, 74, 74)";
+    document.getElementById("homename").style.color = "white";
+    document.getElementById("portfolioname").style.color = "white";
+    document.getElementById("aboutname").style.textDecoration = "underline";
+    document.getElementById("homename").style.textDecoration = "none";
+    document.getElementById("portfolioname").style.textDecoration = "none";
+}
+
+function portfolio(){
+    document.getElementById("portfolioname").style.color ="rgb(245, 74, 74)";
+    document.getElementById("aboutname").style.color = "white";
+    document.getElementById("homename").style.color = "white";
+    document.getElementById("portfolioname").style.textDecoration = "underline";
+    document.getElementById("aboutname").style.textDecoration = "none";
+    document.getElementById("homename").style.textDecoration = "none";
+}
